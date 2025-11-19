dfad16925b44a55f00ec6a4ca05cad37af2d196e
tiffanywoolbright <tiffany.woolbright@us.abb.com>
2025-11-19 15:09:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 
-from snowflake.snowpark.funtions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
